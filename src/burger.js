ad835faffce41eb1e8833640e07d29a7bf2f3b2f
Sergey-hub02 <s.park190802@gmail.com>
2021-10-06 17:09:54 +0300

    Сделал шапку сайта для страниц планет. Она адаптирована более менее и реализовано поведение выпадающего списка

--- src/burger.js
+++ src/burger.js
@@ -1,5 +1,29 @@
+/**
+ * Содержит код, отвечающий за правильное поведение меню в шапке сайта
+ */
 "use strict"
 
-document.querySelector("#burger_menu").addEventListener("click", () => {
-  document.querySelector("#nav").classList.toggle("active");
+const menu = document.querySelector("#nav_menu");
+const planets = document.querySelector("#nav_planets");
+
+menu.addEventListener("click", event => {
+  const node = event.target;
+  node.classList.toggle("active");      // переключаем состояние кнопки меню
+
+  if (node.className.includes("active")) {    // если кнопка находится в активном состоянии, нужно отобразить список планет
+    planets.style.display = "block";
+    return;
+  }
+
+  // иначе скрыть список
+  planets.style.display = "none";
 });
+
+// при клике в любое место страницы, кроме списка планет, нужно скрыть сам список
+document.body.addEventListener("click", event => {
+  if (!event.target.className.includes("nav__button")) {
+    menu.classList.remove("active");
+    planets.style.display = "none";
+  }
+});
+
