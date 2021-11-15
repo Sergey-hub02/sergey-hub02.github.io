9724024a7ffd9465768aa2a19e341388ad873f9e
Sergey-hub02 <s.park190802@gmail.com>
2021-11-16 00:06:32 +0300

    Сделал слайдер для показа планет, может быть добавлю название планеты в качестве заголовка для слайда

--- src/slider.js
+++ src/slider.js
@@ -1,2 +1,41 @@
 "use strict"
 
+const slides = document.querySelectorAll(".slider__item");
+const sliderInner = document.querySelector("#slider-inner");
+const wrapper = document.querySelector(".slider__wrapper");
+
+const slideWidth = 100 / slides.length;
+
+let slideIndex = 0;
+let wrapperWidth;
+
+const init = () => {
+  wrapperWidth = wrapper.offsetWidth;
+  sliderInner.style.width = slides.length * wrapperWidth + "px";
+  moveInner();
+}
+
+const moveInner = () => {
+  sliderInner.style.left = "-" + slideIndex * wrapperWidth + "px";
+}
+
+window.addEventListener("resize", init);
+
+init();
+slides.forEach(slide => {
+  slide.style.flexBasis = slideWidth + "%";
+});
+
+document.querySelector("#button-prev").addEventListener("click", () => {
+  if ((--slideIndex) < 0)
+    slideIndex = slides.length - 1;
+
+  moveInner();
+});
+
+document.querySelector("#button-next").addEventListener("click", () => {
+  if ((++slideIndex) >= slides.length)
+    slideIndex = 0;
+
+  moveInner();
+});
