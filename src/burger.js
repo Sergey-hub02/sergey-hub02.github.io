7d86150ee711f3791d688a47d3870fcfe7f1a801
Sergey-hub02 <s.park190802@gmail.com>
2021-11-16 22:43:38 +0300

    Сделал шапку сайта: она адаптивная, есть анимация перехода из бургер меню в крестик, навигация рабочая

--- src/burger.js
+++ src/burger.js
@@ -3,30 +3,7 @@
  */
 "use strict"
 
-const menu = document.querySelector("#nav_menu");
-const planets = document.querySelector("#nav_planets");
-
-menu.addEventListener("click", event => {
-  const node = event.target;
-  node.classList.toggle("active");      // переключаем состояние кнопки меню
-
-  if (node.className.includes("active")) {    // если кнопка находится в активном состоянии, нужно отобразить список планет
-    planets.style.display = "block";
-    node.firstElementChild.className = "arrow up";
-    return;
-  }
-
-  // иначе скрыть список
-  planets.style.display = "none";
-  node.firstElementChild.className = "arrow down";
+document.querySelector("#burger-menu").addEventListener("click", event => {
+  document.querySelector("#nav").classList.toggle("active");
+  event.target.classList.toggle("active");
 });
-
-// при клике в любое место страницы, кроме списка планет, нужно скрыть сам список
-document.body.addEventListener("click", event => {
-  if (!event.target.className.includes("nav__button")) {
-    menu.classList.remove("active");
-    planets.style.display = "none";
-    menu.firstElementChild.className = "arrow down";
-  }
-});
-
