1b64c0f149e73789fb1a982400757b3573772c8d
Sergey-hub02 <s.park190802@gmail.com>
2021-12-05 12:54:35 +0300

    Внёс финальные правки: убрал ненужные стили, удалил ненужные файлы, изменил текст в подвале сайта

--- src/slider.js
+++ src/slider.js
@@ -4,17 +4,24 @@ const slides = document.querySelectorAll(".slider__item");
 const sliderInner = document.querySelector("#slider-inner");
 const wrapper = document.querySelector(".slider__wrapper");
 
+// расчёт длины слайдов в процентах
 const slideWidth = 100 / slides.length;
 
 let slideIndex = 0;
 let wrapperWidth;
 
+/**
+ * Определяет длину слайдера при изменении размера окна
+ */
 const init = () => {
   wrapperWidth = wrapper.offsetWidth;
   sliderInner.style.width = slides.length * wrapperWidth + "px";
   moveInner();
 }
 
+/**
+ * Определяет текущий слайд
+ */
 const moveInner = () => {
   sliderInner.style.left = "-" + slideIndex * wrapperWidth + "px";
 }
